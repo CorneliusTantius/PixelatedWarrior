978a1ea8bffe9666fdfdc5666cd5d536a9e862c8
cornelius tantius <corneliustantius.ct@gmail.com>
2021-11-10 23:58:58 +0700

    feat: added base header

---
+++ resources/views/Components/header.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <span class="fs-4">Pixelated Warrior</span>
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        </ul>
+    </header>
+</div>
--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -12,11 +12,15 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     {{-- End Region Additional Configurations --}}
+    <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
     <link rel="stylesheet" href="{{$customstyle}}">
     
     <title>PW: {{$title}}</title>
 </head>
 <body>
-    @yield('content')
+    @include('Components.header')
+    <div class="container content-container">
+        @yield('content')
+    </div>
 </body>
 </html>
