14f15e03aa43f4385246f74194aae255e9a01eca
Cornelius Tantius <corneliustantius.ct@gmail.com>
2022-01-12 19:38:26 +0700

    feat: add session functionalities

--- app/Http/Controllers/Auth/LocalAuthController.php
+++ app/Http/Controllers/Auth/LocalAuthController.php
@@ -6,35 +6,41 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\URL;
 
 class LocalAuthController extends Controller
 {
-    public function IndexLogin(){
-        if(Auth::check())
-            return view('Pages/home', ["title"=>"Home"]);
+    public function IndexLogin()
+    {
+        if (Auth::check())
+            return view('Pages/home', ["title" => "Home"]);
         else
-            return view('Pages/Auth/Login',[
+            return view('Pages/Auth/Login', [
                 "title" => "Login",
                 "customstyle" => URL::asset('css/Auth/Login.css')
             ]);
     }
-    public function IndexRegister(){
-        if(Auth::check())
-            return view('Pages/home', ["title"=>"Home"]);
+    public function IndexRegister()
+    {
+        if (Auth::check())
+            return view('Pages/home', ["title" => "Home"]);
         else
-            return view('Pages/Auth/Regist',[
+            return view('Pages/Auth/Regist', [
                 "title" => "Register",
                 "customstyle" => URL::asset('css/Auth/Regist.css')
             ]);
     }
 
-    public function RegisterValidator(Request $request){
-        $validatedData =  $request->validate(request(),[
-            'name'=>['required','min:3','max:22'],
-            'age'=>['required','numeric', 'min:17'],
-            'email'=>['required','email:dns','unique:users'],
-            'password'=>['required','regex:/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/','min:8']
+    public function RegisterValidator(Request $request)
+    {
+        dd($request);
+        $validatedData =  $request->validate(request(), [
+            'name' => ['required', 'min:3', 'max:22'],
+            'age' => ['required', 'numeric', 'min:17'],
+            'email' => ['required', 'email:dns', 'unique:users'],
+            'password' => ['required', 'regex:/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/', 'min:8']
         ]);
 
         $validatedData['password'] = bcrypt($validatedData['password']);
@@ -42,24 +48,32 @@ class LocalAuthController extends Controller
         User::create($validatedData);
 
         request()->session()->flash('success', 'Registration successfull!');
-        
+
         return redirect()->to('/');
     }
-    public function LoginValidator(Request $request){
-        $credentials =$request->validate([
-            'email'=>['required','email:dns'],
-            'password'=>['required']
+    public function LoginValidator(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email:dns'],
+            'password' => ['required']
         ]);
-        
-        if(Auth::attempt($credentials)){
+        if ($request['remember']) {
+            Cookie::queue('email', $request['email'], 120);
+            Cookie::queue('password', $request['password'], 120);
+        }
+        if (Auth::attempt($credentials)) {
+            $user = Auth::User();
+            Session::put('session', $credentials);
+            Session::put('user', $user);
             $request->session()->regenerate();
-            return redirect()->intended('/')->with('success','Login Success');
+            return redirect()->intended('/')->with('success', 'Login Success');
         }
 
         return redirect()->to('/login');
     }
 
-    public static function Logout(Request $request){
+    public static function Logout(Request $request)
+    {
         Auth::logout();
         return redirect('/');
     }
--- resources/views/Pages/Auth/Login.blade.php
+++ resources/views/Pages/Auth/Login.blade.php
@@ -1,31 +1,39 @@
 @extends('Layouts.main')
 @section('content')
-<div class="form-signin">
-    <form method="POST" action="/auth/local/login">
-        @csrf
-        {{-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
-        <h1 class="h3 mb-3 fw-normal">Login</h1>
-    
-        <div class="form-floating">
-            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="name@example.com" required value="{{ old('email') }}">
-            <label for="floatingInput">Email address</label>
-            @error('email')
-                <div class="invalid-feedback error-message">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-        <div class="form-floating">
-            <input type="password" class="form-control" id="password" placeholder="Password" name="password" required value="{{ old('password') }}">
-            <label for="floatingPassword">Password</label>
-        </div>
-    
-        <div class="checkbox mb-3 mt-3">
-            <span class="fw-light">Don't have an account yet? <a href="/register">Register Here!</a></span>
-        </div>
-        <button class="w-100 btn btn-lg btn-success" type="submit">Login</button>
-        <p class="mt-0 mb-0 text-muted text-center">OR</p>
-        <a class="w-100 btn btn-lg btn-primary" href="/auth/google/redirect">Login with Google</a>
-    </form>
-</div>
+    <div class="form-signin">
+        <form method="POST" action="/auth/local/login">
+            @csrf
+            {{-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
+            <h1 class="h3 mb-3 fw-normal">Login</h1>
+
+            <div class="form-floating">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email"
+                    placeholder="name@example.com" required value="{{ old('email') }}">
+                <label for="floatingInput">Email address</label>
+                @error('email')
+                    <div class="invalid-feedback error-message">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="password" placeholder="Password" name="password" required
+                    value="{{ old('password') }}">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember">
+
+                <label class="form-check-label" for="remember">
+                    {{ __('Remember Me') }}
+                </label>
+            </div>
+            <div class="checkbox mb-3 mt-3">
+                <span class="fw-light">Don't have an account yet? <a href="/register">Register Here!</a></span>
+            </div>
+            <button class="w-100 btn btn-lg btn-success" type="submit">Login</button>
+            <p class="mt-0 mb-0 text-muted text-center">OR</p>
+            <a class="w-100 btn btn-lg btn-primary" href="/auth/google/redirect">Login with Google</a>
+        </form>
+    </div>
 @endsection
